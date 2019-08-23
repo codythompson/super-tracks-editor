26f747dea06f49690a21fcc06ad049c080639cfa
Cody Thompson <cody.thompson22@gmail.com>
2019-08-22 23:46:55 -0700

    Tmp/atlas validate (#1)
    Added Atlas._validate function and tests

--- src/Atlas.js
+++ src/Atlas.js
@@ -4,6 +4,8 @@ const CONNECTION_SYMBOLS = {
   ACTIVE_CONNECTION: '*'
 }
 
+class AtlasParseError extends Error {}
+
 class Atlas {
   static _removeEmptyLines(atlasContentArray) {
     let i = 0;
@@ -21,6 +23,35 @@ class Atlas {
     return atlasContentArray
   }
 
+  static _validate(atlasContentArray) {
+    if (atlasContentArray.length === 0) {
+      throw new AtlasParseError('Atlas file is empty')
+    }
+    if (atlasContentArray.length % 4 !== 3) {
+      throw new AtlasParseError('Malformed rows: invalid number of lines (whitespace only lines preceding or following content ok)')
+    }
+    if (atlasContentArray[0].length % 4 !== 3) {
+      throw new AtlasParseError('Malformed columns: invalid number of characters (whitespace only lines preceding or following content ok)')
+    }
+    let charWidth = atlasContentArray[0].length
+    let horSepTest = /^-+$/
+    let lineCharTest = /^((o| |\*){3}\|{1})*((o| |\*){3}){1}$/
+    for (let i = 0; i < atlasContentArray.length; i++) {
+      let line = atlasContentArray[i]
+      if (line.length !== charWidth) {
+        throw new AtlasParseError(`Malformed column: more or less characters than other lines: line ${i+1}`)
+      }
+      if (i % 4 === 3 && !horSepTest.test(line)) {
+        throw new AtlasParseError(`Malformed row, expecting separator line (a bunch of hyphens -------): line ${i+1}`)
+      }
+      if (i % 4 !== 3 && !lineCharTest.test(line)) {
+        throw new AtlasParseError(`Malformed row, unexpected chars: line ${i+1}`)
+      }
+    }
+  }
+
+  // TODO: getTileObj(atlasContent, column, row)
+
   /**
    * takes the contents of an atlas file and returns an Atlas instance.
    * @param {string|Array.<string>} altasContent the contents of and atlas file
@@ -33,4 +64,4 @@ class Atlas {
 }
 
 export default Atlas
-export { CONNECTION_SYMBOLS }
+export { CONNECTION_SYMBOLS, AtlasParseError }
--- src/Atlas.test.js
+++ src/Atlas.test.js
@@ -1,4 +1,4 @@
-import Atlas from './Atlas'
+import Atlas, { AtlasParseError } from './Atlas'
 
 test('Atlas._removeEmptyLines should remove empty lines', () => {
   const noEmptyLines =
@@ -65,4 +65,106 @@ blah     cooooontent   \t\t
   expect(Atlas._removeEmptyLines(endEmptyLine)).toEqual(endEmptyLineExpected)
   expect(Atlas._removeEmptyLines(multipleEmpties)).toEqual(multipleEmptiesExpected)
   expect(Atlas._removeEmptyLines(multipleEmpties)).toEqual(multipleEmptiesExpected)
+})
+
+test('Atlas._validate should throw errors when stuff ins\'t valid', () => {
+  const validA =
+`o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+-------------------
+o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|ooo| o `
+    .split('\n')
+  const validB =
+`o *
+***
+ o `
+    .split('\n')
+  const empty = []
+  const wrongLineCountA =
+`o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+-------------------`
+    .split('\n')
+  const wrongLineCountB =
+`o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+-------------------
+o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+-------------------
+o *|   |***|ooo| o `
+    .split('\n')
+  const wrongCharCount =
+`o *|   |***|o oo| o 
+o *|   |***|o oo| o 
+o *|   |***|o oo| o 
+-------------------
+o *|   |***|o oo| o 
+o *|   |***|o oo| o 
+o *|   |***|o oo| o `
+    .split('\n')
+  const wrongLine =
+`o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|oooo| o 
+-------------------
+o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|ooo| o `
+    .split('\n')
+  const wrongHorSepA =
+`o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+------------- -----
+o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|ooo| o `
+    .split('\n')
+  const wrongHorSepB =
+`o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|ooo| o 
+-------------------
+o *|   |***|ooo| o 
+o *|   |***|ooo| o `
+    .split('\n')
+  const wrongCharA =
+`o *|   |***|ooo| o 
+o *|   |***|ooa| o 
+o *|   |***|ooo| o 
+-------------------
+o *|   |**|*ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|ooo| o `
+    .split('\n')
+  const wrongCharB =
+`o *|   |***|ooo| o 
+o *|   |***|oo | o 
+o *|   |***|ooo| o 
+-------------------
+o *|   |**|*ooo| o 
+o *|   |***|ooo| o 
+o *|   |***|ooo| o `
+    .split('\n')
+
+    expect(Atlas._validate(validA)).toBeUndefined()
+    expect(Atlas._validate(validB)).toBeUndefined()
+    expect(() => Atlas._validate(empty)).toThrowWithMessage(AtlasParseError, 'Atlas file is empty')
+    expect(() => Atlas._validate(wrongLineCountA)).toThrowWithMessage(AtlasParseError, 'Malformed rows: invalid number of lines (whitespace only lines preceding or following content ok)')
+    expect(() => Atlas._validate(wrongLineCountB)).toThrowWithMessage(AtlasParseError, 'Malformed rows: invalid number of lines (whitespace only lines preceding or following content ok)')
+    expect(() => Atlas._validate(wrongCharCount)).toThrowWithMessage(AtlasParseError, 'Malformed columns: invalid number of characters (whitespace only lines preceding or following content ok)')
+    expect(() => Atlas._validate(wrongLine)).toThrowWithMessage(AtlasParseError, 'Malformed column: more or less characters than other lines: line 3')
+    expect(() => Atlas._validate(wrongLine)).toThrowWithMessage(AtlasParseError, 'Malformed column: more or less characters than other lines: line 3')
+    expect(() => Atlas._validate(wrongHorSepA)).toThrowWithMessage(AtlasParseError, 'Malformed row, expecting separator line (a bunch of hyphens -------): line 4')
+    expect(() => Atlas._validate(wrongHorSepB)).toThrowWithMessage(AtlasParseError, 'Malformed row, expecting separator line (a bunch of hyphens -------): line 4')
+    expect(() => Atlas._validate(wrongCharA)).toThrowWithMessage(AtlasParseError, 'Malformed row, unexpected chars: line 2')
+    expect(() => Atlas._validate(wrongCharB)).toThrowWithMessage(AtlasParseError, 'Malformed row, unexpected chars: line 5')
 })
