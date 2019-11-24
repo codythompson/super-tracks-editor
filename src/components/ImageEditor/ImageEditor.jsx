84fe33b5faae014318947d492d7c30a38cb7691f
Cody Thompson <cody.thompson22@gmail.com>
2019-11-24 09:28:52 -0800

    SVG creation basic implementation working

--- src/components/ImageEditor/ImageEditor.jsx
+++ src/components/ImageEditor/ImageEditor.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import ControlBar from './ImageEditorControlBar'
+import TileSVG from './TileSVG'
+import {CONNECTIONS} from '../../TileInfo'
 
 import styles from '../../styles/ImageEditor/ImageEditor.module.scss'
 
@@ -14,6 +16,9 @@ export default class extends React.Component {
       <div className={styles.ImageEditor}>
         <div className={styles.ControlBarContainer}>
           <ControlBar />
+        </div>
+        <div className={styles.ImageCanvasContainer}>
+          <TileSVG connection={CONNECTIONS.LEFT_TOP} />
         </div>
       </div>
   )
